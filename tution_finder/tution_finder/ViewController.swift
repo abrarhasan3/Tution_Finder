f498d6ca15f471b53dea980f74cfd20c2edf7989
Abrar <abrarhasan1998@gmail.com>
2022-11-02 18:41:00 +0600

    Login and SignUp screen created

---
+++ tution_finder/tution_finder/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  tution_finder
+//
+//  Created by kuet on 2/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var signuptv: UITextView!
+    
+    override func viewDidLoad() {
+        //signuptv.layer.setBorder
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
